8c1b4d493225a0b924c4f890ee270a1938634e5c
Rohit <rohit.verma@itsrizzoli.it>
2024-07-01 21:48:31 +0200

    Added bottom card

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import {TextInput, Text, View, StyleSheet, Button, ImageBackground} from "react-native"
+import {ImageBackground, Text, View} from "react-native"
 import {router, Stack} from "expo-router";
 // @ts-ignore
 import backgroundImage from './flusso-pagamento/assets/parcheggi.png';
 import {style} from "@/app/flusso-pagamento/Components/Stili"
-import { FontAwesome5 } from '@expo/vector-icons';
+import {FontAwesome5} from '@expo/vector-icons';
 import {Pressable} from "expo-router/build/views/Pressable";
 
 
@@ -19,15 +19,50 @@ const Index = () => {
                     title: "Home",
                 }}
             />
-            <ImageBackground source={backgroundImage} resizeMode="cover" style={style.bg_style} />
+            <ImageBackground source={backgroundImage} resizeMode="cover" style={style.bg_style}/>
             <View style={style.starting_btn}>
-                <Pressable onPress={() => router.push("/flusso-pagamento/Pagina-1")} style={{display: "flex", flexDirection: "row", alignItems: "center", gap: 8}} >
-                    <FontAwesome5 name="map-marker-alt" size={42} color="red" />
-                    <Text style={{fontSize: 24, fontWeight: "bold", color: "white",  textShadowColor:'#000000',
-                        textShadowRadius:2,}}>
+                <Pressable onPress={() => router.push("/flusso-pagamento/Pagina-1")}
+                           style={{display: "flex", flexDirection: "row", alignItems: "center", gap: 8}}>
+                    <FontAwesome5 name="map-marker-alt" size={42} color="red"/>
+                    <Text style={{
+                        fontSize: 24, fontWeight: "bold", color: "white", textShadowColor: '#000000',
+                        textShadowRadius: 2,
+                    }}>
                         Parcheggio SanGiovanni
                     </Text>
                 </Pressable>
+                <View style={{
+                    position: "absolute",
+                    height: 200,
+                    width: "100%",
+                    borderTopRightRadius: 69,
+                    borderTopLeftRadius: 69,
+                    backgroundColor: "white",
+                    bottom: 50,
+                    shadowRadius: 69,
+                    shadowColor: "black",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    gap: 8
+                }}>
+                    <Text style={{color: "#3895ff", fontWeight: "bold", fontSize: 32}}>Sei arrivato!</Text>
+                    <Pressable onPress={() => router.push("/flusso-pagamento/Pagina-1")}
+                               style={{display: "flex", flexDirection: "row", alignItems: "center", gap: 8}}>
+                        <Text style={{
+                            fontSize: 24,
+                            fontWeight: "bold",
+                            color: "white",
+                            textShadowColor: '#000000',
+                            textShadowRadius: 2,
+                            backgroundColor: "#3895ff",
+                            padding: 8,
+                            paddingHorizontal: 16,
+                            borderRadius: 16
+                        }}>
+                            Vai al pagamento
+                        </Text>
+                    </Pressable>
+                </View>
             </View>
         </View>
     )
